c9f1e460fc884d3f70210e4d06f5c229e420920e
JoaoCardosoDev <jmcardoso90@gmail.com>
2024-01-18 22:00:49 +0000

    Feat: Class

---
+++ aula4/script.js
@@ -0,0 +1,95 @@
+
+// let a = 1;
+// let b = 0;
+
+// const fibonacci = () => {
+// // iterating 10 times
+//     for (let i = 0; i < 10; i++) {
+//         const temp = a;
+//         a = a + b;
+//         b = temp;
+//         console.log(a); 
+//     }
+// }
+
+// fibonacci();
+
+ let user = {
+
+     id: 0,
+     name: 'João',
+     age: 31,
+     gender: 'male',
+     dob: '01-01-1990',
+     hobbies: ['Loitering', 'Arson', 'Grand Theft Auto', 'Gerrymandering']
+}
+
+// for (const key in user) {
+//     console.log(user[key])
+// }
+
+// console.log(user);
+// console.table(user);
+
+// const originalObject= {
+//     key1: "value1",
+//     key2: "value2",
+//     key3: "value3",
+// };
+
+// const entriesArray= Object.entries(originalObject);
+
+// console.log(entriesArray);
+
+// const transformedArray = entriesArray.map([key,value]) => {
+//     return {
+//         originalKey: key;
+//         originalValue: value,
+//         transformedValue: valuetoUpperCase(),
+//     };
+// }
+// console.log(transformedArray)
+
+// class User {
+
+//     #name;
+//     #age;
+
+//     constructor() {
+//         this.#name = user.name;
+//         this.#age = user.age;
+//         console.log("Class user initiated");
+//         console.log(this.#getName());
+//     }
+
+//     #getName() {    
+//         // console.log("Return the user name");
+//         return this.name;
+//     }
+
+//     getAge() {
+//         // console.log("Return the user age");
+//         return this.age;
+//     }
+
+//     // set name(value){
+//     //     this.#name= value;
+//     // }
+//     get name() {
+//         return this.#name;
+//     }
+// }
+
+// const user2 = new User(user);
+// // user2.name = user.name;
+// // user2.age = user.age;
+// user2.name = "Tony"
+// console.log(user2.name);
+// console.log(user2.age);
+// console.log(user.getName())
+
+
+
+// console.log(Car);
+
+
